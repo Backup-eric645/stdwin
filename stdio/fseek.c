9e13de8497001bd9cbfeae12476c0cfc736c5613
Backup-eric645 <52118151+Backup-eric645@users.noreply.github.com>
2020-11-15 13:26:03 -0500

    Update fseek.c

--- stdio/fseek.c
+++ stdio/fseek.c
@@ -1,8 +1,7 @@
 #include <windows.h>
-typedef void* Handle;
 typedef int DWord;
 typedef unsigned int UDWord;
-int fseek(Handle hFile, LARGE_INTEGER moveDist, DWord seekMode)
+int fseek(HANDLE hFile, LARGE_INTEGER moveDist, DWord seekMode)
 {
     return SetFilePointerEx(hFile, moveDist, 0, seekMode - 1);
 }
